d15e8655b8cd6a5d033c8879a31bd13e8f4c3b52
Andy <andy202ochoa@gmail.com>
2026-09-21 12:12:54 -0500

    resolviendo error 404

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,7 @@ export default function App() {
   if (!user) {
     return (
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
           path="/login"
           element={<LoginPage onSwitchToRegister={() => navigate('/register')} />}
